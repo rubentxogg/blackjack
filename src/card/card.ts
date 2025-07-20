e5d1a235b9a70d47988921f048263633b128bbe4
rubentxogg <rubentxogg@hotmail.com>
2025-07-20 17:12:39 +0200

    [ADD] Card values dictionary

--- src/card/card.ts
+++ src/card/card.ts
@@ -6,9 +6,29 @@ export class Card {
     readonly suit: Suit;
     readonly face: string;
 
+    private readonly dictionary = new Map([
+        [Rank.ACE, 11],
+        [Rank.KING, 10],
+        [Rank.QUEEN, 10],
+        [Rank.JACK, 10],
+        [Rank.TEN, 10],
+        [Rank.NINE, 9],
+        [Rank.EIGHT, 8],
+        [Rank.SEVEN, 7],
+        [Rank.SIX, 6],
+        [Rank.FIVE, 5],
+        [Rank.FOUR, 4],
+        [Rank.THREE, 3],
+        [Rank.TWO, 2],
+    ]);
+
     constructor(rank: number, suit: Suit) {
         this.rank = rank;
         this.suit = suit;
         this.face = `${rank}-${suit}.svg`;
+    }
+
+    protected getValue(): number {
+        return this.dictionary.get(this.rank) ?? 0;
     }
 }
